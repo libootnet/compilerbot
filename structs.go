edf593fa6f57491580182de45eedfb461c5b87c1
libootnet <alefudiscord@gmail.com>
2024-11-12 18:14:17 +0900

    docker latest

--- structs.go
+++ structs.go
@@ -1,11 +1,11 @@
 package main
 
 var LanguageTypes = map[string]string{
-	"py":   "docker.io/library/python:3.13", // Python
-	"go":   "docker.io/library/golang:1.23", // Go
-	"java": "docker.io/library/openjdk:18",  // Java (OpenJDK 17)
-	"js":   "docker.io/library/node:20",     // Node.js
-	"rb":   "docker.io/library/ruby:3.1",    // Ruby
-	"php":  "docker.io/library/php:8.0",     // PHP
+	"py":   "docker.io/library/python:3.13",    // Python
+	"go":   "docker.io/library/golang:latest",  // Go
+	"java": "docker.io/library/openjdk:latest", // Java (OpenJDK 17)
+	"js":   "docker.io/library/node:20",        // Node.js
+	"rb":   "docker.io/library/ruby:latest",    // Ruby
+	"php":  "docker.io/library/php:8.2-cli",    // PHP
 	// "rs":   "docker.io/library/rust:1.80",   // Rust
 }
