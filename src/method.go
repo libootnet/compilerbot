e7f517b86ffb902161a296e56dade76a2b5bd6d9
libootnet <alefudiscord@gmail.com>
2024-11-13 18:25:09 +0900

    fix method

--- src/method.go
+++ src/method.go
@@ -6,33 +6,45 @@ import (
 	"strings"
 )
 
+func Cargo(path string) []string {
+	return []string{"sh", "-c", fmt.Sprintf("cargo new project > /dev/null 2>&1 && cp %s project/src/main.rs && cd project && cargo build --release > /dev/null 2>&1 && ./target/release/project", path)}
+}
+
+func Go(path string) []string {
+	return []string{"sh", "-c", `mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 1777 "$GOPATH" && go run ` + path}
+}
+
+func C_CPP(gcc, path string) []string {
+	return []string{"sh", "-c", fmt.Sprintf("%s %s -o /scripts/%s && ./scripts/%s", gcc, path, BaseName(path), BaseName(path))}
+}
+
 func Methods(language, path string) []string {
 	switch language {
 	case "py":
 		return []string{"python", path}
 	case "go":
-		return []string{"sh", "-c", `mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 1777 "$GOPATH" && go run ` + path}
+		return Go(path)
 	case "java":
 		return []string{"java", path}
 	case "js":
 		return []string{"node", path}
 	case "rs":
 		// debug in rust language
-		return []string{"sh", "-c", fmt.Sprintf("cargo new project > /dev/null 2>&1 && cp %s project/src/main.rs && cd project && cargo build --release > /dev/null 2>&1 && ./target/release/project", path)}
+		return Cargo(path)
 	case "rb":
 		return []string{"rb", path}
 	case "php":
 		return []string{"php", path}
 	case "c":
-		return []string{"sh", "-c", fmt.Sprintf("gcc %s -o /scripts/%s && ./scripts/%s", path, GetBaseName(path), GetBaseName(path))}
+		return C_CPP("gcc", path)
 	case "cpp", "cxx", "cc":
-		return []string{"sh", "-c", fmt.Sprintf("g++ %s -o /scripts/%s && ./scripts/%s", path, GetBaseName(path), GetBaseName(path))}
+		return C_CPP("g++", path)
 	default:
 		return []string{}
 	}
 }
 
-func GetBaseName(filePath string) string {
+func BaseName(filePath string) string {
 	fileName := filepath.Base(filePath)
 	baseName := strings.TrimSuffix(fileName, filepath.Ext(fileName))
 	return baseName
