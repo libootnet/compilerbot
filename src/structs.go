2c59e1a113f878d0c9d348ffe09fe91250479e4f
libootnet <alefudiscord@gmail.com>
2024-11-13 14:53:36 +0900

    small fix

--- src/structs.go
+++ src/structs.go
@@ -3,7 +3,7 @@ package src
 var LanguageTypes = map[string]string{
 	"py":   "docker.io/library/python:latest",  // Python
 	"go":   "docker.io/library/golang:latest",  // Go
-	"java": "docker.io/library/openjdk:latest", // Java (OpenJDK 17)
+	"java": "docker.io/library/openjdk:latest", // Java
 	"js":   "docker.io/library/node:latest",    // Node.js
 	"rb":   "docker.io/library/ruby:latest",    // Ruby
 	"php":  "docker.io/library/php:latest",     // PHP
