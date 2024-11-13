4830be268fe5fbcf9ddd18a4ccd67e5695d55379
libootnet <alefudiscord@gmail.com>
2024-11-13 15:48:02 +0900

    fix

--- src/discord.go
+++ src/discord.go
@@ -103,7 +103,7 @@ func MessageContent(s *discordgo.Session, m *discordgo.MessageCreate) {
 				}
 			}
 
-			_, err = s.ChannelMessageEdit(message.Reference().ChannelID, message.Reference().MessageID, fmt.Sprintf("```%s```", output))
+			_, err = s.ChannelMessageEdit(message.Reference().ChannelID, message.Reference().MessageID, fmt.Sprintf("```\n%s```", output))
 			if err != nil {
 				fmt.Println(err)
 				s.ChannelMessageEdit(message.Reference().ChannelID, message.Reference().MessageID, "```To many Requests```")
