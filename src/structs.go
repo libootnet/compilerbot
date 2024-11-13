733edc52412429a204a3f2970a00e9d4f14cf8a2
libootnet <alefudiscord@gmail.com>
2024-11-13 14:29:19 +0900

    languagetype is latest

--- src/structs.go
+++ src/structs.go
@@ -1,11 +1,11 @@
 package src
 
 var LanguageTypes = map[string]string{
-	"py":   "docker.io/library/python:3.13",    // Python
+	"py":   "docker.io/library/python:latest",  // Python
 	"go":   "docker.io/library/golang:latest",  // Go
 	"java": "docker.io/library/openjdk:latest", // Java (OpenJDK 17)
-	"js":   "docker.io/library/node:20",        // Node.js
+	"js":   "docker.io/library/node:latest",    // Node.js
 	"rb":   "docker.io/library/ruby:latest",    // Ruby
-	"php":  "docker.io/library/php:8.2-cli",    // PHP
+	"php":  "docker.io/library/php:latest",     // PHP
 	// "rs":   "docker.io/library/rust:1.80",   // Rust
 }
